155ee33ec1521627015c3fa555d53e2e34a9aba8
Junhyeon <psjking1229@naver.com>
2023-05-22 22:23:34 +0900

    vh

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 document.querySelector('.cancel-btn').addEventListener('click', () => {
   document.querySelector('.toggle-menu').classList.remove('show');
 });
+
+const mainMenuTitles = document.querySelectorAll('.main-menu-title');
+const subMenues = document.querySelectorAll('.sub-menu');
+
+for (let i = 0; i < mainMenuTitles.length; i++) {
+  mainMenuTitles[i].addEventListener('click', () => {
+    subMenues[i].classList.toggle('show');
+  });
+}
